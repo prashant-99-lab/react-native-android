7256e3551ff4c21e914422f83069f70c97e6f76d
prashant-99-lab <chaudharyprashant@mooglelabs.com>
2025-01-23 12:00:06 +0530

    addd

--- App.tsx
+++ App.tsx
@@ -77,7 +77,7 @@ function App(): React.JSX.Element {
             backgroundColor: isDarkMode ? Colors.black : Colors.white,
           }}>
           <Section title="Step One">
-            Editddddd <Text style={styles.highlight}>App.tsx</Text> to change this
+            Editxxx <Text style={styles.highlight}>App.tsx</Text> to change this
             screen and then come back to see your edits.
           </Section>
           <Section title="See Your Changes">
